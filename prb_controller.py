01eaae4eb447eb00ea5e14fd735ae5946615d0cf
Sarah Johnson <sozzifer@gmail.com>
2022-09-29 17:07:05 +0100

    Final deployment

--- prb_controller.py
+++ prb_controller.py
@@ -129,6 +129,6 @@ def set_draw_speed(value):
 
 
 if __name__ == "__main__":
-    # app.run(debug=True)
+    app.run(debug=True)
     # To deploy on Docker, replace app.run(debug=True) with the following:
-    app.run(debug=False, host="0.0.0.0", port=8080, dev_tools_ui=False)
+    # app.run(debug=False, host="0.0.0.0", port=8080, dev_tools_ui=False)
